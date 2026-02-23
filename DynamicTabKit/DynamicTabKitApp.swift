52f6d84a9b8ee08a417f6badbc842e52d747182d
Navin Rai <rai.navinrai20@gmail.com>
2026-02-23 22:37:19 +0530

    Initial Commit

---
+++ DynamicTabKit/DynamicTabKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabKitApp.swift
+//  DynamicTabKit
+//
+//  Created by Navin Rai on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
